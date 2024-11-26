a98f55017933077d2458e69cb3290f352410748f
Rudrashish Palav <rudrashishpalav@gmail.com>
2024-11-26 21:54:59 +0100

    Adding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import Message from './Message'
+import ListGroup from "./components/ListGroup";
 
 function App(){
 
-  const name = "Rudrashish"
-  if (name){
-    return <h1>Hello {name}</h1>;
-  }
-  return <div><Message/></div>;
+  return <div><ListGroup/></div>;
 }
 
 export default App ;
